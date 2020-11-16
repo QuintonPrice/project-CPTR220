87a7729c5ff0a912b0768f08ff3a611caa1089f3
QuintonPrice <quintonp10@gmail.com>
2020-11-15 23:42:13 -0800

    got file paths working

---
+++ cptr-app/src/components/Trails.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Trails extends Component {
+    render() {
+        return(<p>Test</p>);
+    }
+}
+export default Trails;
--- cptr-app/src/index.js
+++ cptr-app/src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'; // imports React
+import ReactDOM from 'react-dom'; 
+import App from './App'; 
+import Trails from './components/Trails';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Trails />, document.getElementById('root')); // renders App, which renders page
