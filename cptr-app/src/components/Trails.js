fb076aa0b3b1b96c33d77da5ce09e4cfcdc62e70
QuintonPrice <quintonp10@gmail.com>
2020-11-16 00:16:54 -0800

    some parts of trails page now working

--- cptr-app/src/components/Trails.js
+++ cptr-app/src/components/Trails.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import '../main.css'
+import '../main.css';
+import './trails.css';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <p>Test</p>
+                <NavBar />
                 <Blurb />
             </div>
-
-
         );
     }
 }
 
+// navigation bar class
+class NavBar extends Component {
+    render() {
+        return (
+            <nav class="navbar navbar-expand-lg navbar-dark navbar-fixed-top">
+                <li><a href="../../index.html">Home</a></li>
+                <li><a href="trails.html">Trails</a></li>
+                <li> <a href="../travel/travel.html">Travel</a></li>
+                <li> <a href="../housing/housing.html">Housing</a></li>
+            </nav>
+        );
+    }
+}
+
+// top blurb class
 class Blurb extends Component {
     render() {
         return (
