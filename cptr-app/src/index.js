cadcd9dec3630ba62823522d61338401d1c7597a
Joel Hartman <65033855+joelhart@users.noreply.github.com>
2020-11-16 15:33:30 -0800

    stage-6-homepage-push

--- cptr-app/src/index.js
+++ cptr-app/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import HomeApp from './components/Home.js'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <HomeApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
