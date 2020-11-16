675d3735a6e2b0355d57129096e154a7889509e9
QuintonPrice <quintonp10@gmail.com>
2020-11-15 23:57:39 -0800

    trying to fix issue with main.css

--- cptr-app/src/components/Trails.js
+++ cptr-app/src/components/Trails.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import '../main.css'
 
-class Trails extends Component {
+class App extends Component {
     render() {
-        return(<p>Test</p>);
+        return (
+            <div>
+                <p>Test</p>
+                <Blurb />
+            </div>
+
+
+        );
     }
 }
-export default Trails;
+
+class Blurb extends Component {
+    render() {
+        return (
+            <div className="container" id="blurb">
+                <h2>Trails in the Pacific Northwest</h2>
+                <hr></hr>
+                <p>With hundreds of trails to choose from, the Pacific Northwest undoubtedly has some of the best
+                mountain biking in the world. Below is a list with information about some of my personal favorites,
+                as well as rides I have on my bucket list!</p>
+            </div>
+        );
+    }
+}
+
+export default App;
--- cptr-app/src/index.js
+++ cptr-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'; // imports React
 import ReactDOM from 'react-dom'; 
-import App from './App'; 
+import App from './components/Trails';
 import Trails from './components/Trails';
 
-ReactDOM.render(<Trails />, document.getElementById('root')); // renders App, which renders page
+ReactDOM.render(<App />, document.getElementById('root')); // renders App, which renders page
