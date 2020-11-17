f57c32dbb5222f16faa72dec34b9206d30825fd4
QuntonPrice <quintonp10@gmail.com>
2020-11-16 22:50:37 -0800

    added semantic content for trails.js

--- cptr-app/src/App.js
+++ cptr-app/src/App.js
@@ -1,13 +1,24 @@
+'use strict'
 import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div >
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-        </p>
-    </div>
-  );
+class DarkMode extends Component {
+  // sets state, used to track if dark mode is on
+  state = {
+    darkModeEnable: false
+  }
+
+  // toggles darkModeEnable state
+  toggleDarkMode = () => {
+    this.setState({
+      darkModeEnable: !this.state.darkModeEnable
+    });
+    console.log(this.state.darkModeEnable);
+  }
+
+  // renders clickable button
+  render() {
+    return <button onClick={this.toggleDarkMode} className="button btn-dark" id="dark_mode">Dark Mode</button>
+  }
 }
 
-export default App;
+export default DarkMode; // exports DarkMode
--- cptr-app/src/index.js
+++ cptr-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'; // imports React
 import ReactDOM from 'react-dom'; 
-import App from './components/Trails';
-import Trails from './components/Trails';
+import Trails from './components/Trails'; // trails page
 
-ReactDOM.render(<App />, document.getElementById('root')); // renders App, which renders page
+// renders Trail class, which renders pag
+ReactDOM.render(<Trails />, document.getElementById('root')); 
