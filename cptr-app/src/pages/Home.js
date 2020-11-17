f5e5f89a152f066170b988b995d706b34884d431
QuintonPrice <quintonp10@gmail.com>
2020-11-17 14:29:27 -0800

    Updated Home.js

--- cptr-app/src/pages/Home.js
+++ cptr-app/src/pages/Home.js
@@ -7,33 +7,17 @@ import '../main.css'; // main css file
 // import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HomeApp extends Component {
+class Home extends Component {
     render() {
         return (
             <div classname="container">
-                <section id="overview">
-                    <h2> Overview </h2>
-                    <hr />
-                    <HomeDesc />
-                </section>
-            </div>
-        )
-    }
-}
-
-class HomeDesc extends Component {
-    render() {
-        return (
-            <div className="container">
-                <p> Content goes here </p>
-                
+                <p>Put content here</p>
             </div>
         )
     }
 }
 
 
-export default HomeApp;
 
-//This might need to go in a different file
-ReactDOM.render(<HomeApp />, document.getElementById('root'));
+
+export default Home;
